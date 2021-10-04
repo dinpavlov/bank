70048fc58a52cdc192fbce47e314d42f1a3f351d
denpa16 <den.pavlov.1998@list.ru>
2021-10-04 11:49:00 +0300

    create account create tests

--- app/billing/tests.py
+++ app/billing/tests.py
@@ -23,4 +23,24 @@ class AccountApiTests(APITestCase):
     def test_get_account(self):
         res = self.client.get(reverse('billing:account_get', args=[self.account.id]))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        
+
+    def test_create_account(self):
+        data = {
+            "account_name": self.account.account_name,
+            "overdraft": True
+        }
+        res = self.client.post(reverse('billing:account_create'), data)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        account = Account.objects.get(id=res.data['id'])
+        for key in data.keys():
+            self.assertEqual(data[key], getattr(account, key))
+
+    def test_create_default_overdraft_account(self):
+        data = {
+            "account_name": self.account.account_name,
+        }
+        res = self.client.post(reverse('billing:account_create'), data)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        account = Account.objects.get(id=res.data['id'])
+        for key in data.keys():
+            self.assertEqual(data[key], getattr(account, key))

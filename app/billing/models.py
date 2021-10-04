9dd14b90f686b933b95e2e45e82120ce6a916102
denpa16 <den.pavlov.1998@list.ru>
2021-10-04 11:04:12 +0300

    create account model

--- app/billing/models.py
+++ app/billing/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Account(models.Model):
+    account_name = models.CharField(max_length=100)
+    balance = models.DecimalField(
+        default=0,
+        max_digits=12,
+        decimal_places=2
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT  # we cannot delete user with money
+    )
+
+    overdraft = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return self.id

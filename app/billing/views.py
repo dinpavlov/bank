9f885fef8cc1c317115db31df08c70d4d6b401b9
denpa16 <den.pavlov.1998@list.ru>
2021-10-04 11:13:09 +0300

    update account get view

--- app/billing/views.py
+++ app/billing/views.py
@@ -1,8 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Account
+from .serializers import AccountSerializer
 
 
 @api_view(['GET'])
 def account_get(request, id):
-    return Response('account_get_endpoint')
+    try:
+        account = Account.objects.get(id=id)
+    except Account.DoesNotExist:
+        return Response('Account with id={} does not exist'.format(id),status=status.HTTP_404_NOT_FOUND)
+    serializer = AccountSerializer(account)
+    return Response({'balance': serializer.data['balance']}, status=status.HTTP_200_OK)
